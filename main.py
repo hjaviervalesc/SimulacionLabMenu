98873075a90e50d1337c08ef15933c7b0088b8dd
Olaia Picabea <72024925+olaaaiap@users.noreply.github.com>
2026-03-27 16:48:29 +0100

    UPDATE

--- main.py
+++ main.py
@@ -9,17 +9,32 @@ total_time = 5
 
 
 #Hacerlo con forward euler
-dt = 0.01
+dt = 1
 t = 0.0
 t0 = 0.0
-v_func = lambda t: v0 + g * t
-x_func = lambda t:x0 + v0 * (t - t0) + 0.5 * g * (t - t0)**2
+# v_func = lambda t: 
+v_prev = v0
+x_prev = x0
 
+def v_func(t):
+    vtdt = v_prev + g * dt
+    return vtdt
+    
 
-dt = 0.01
+def x_func(t):
+    xtdt = x_prev + v_prev * dt
+    return xtdt
+
+# x_func = lambda t: 
+
 
 while t < total_time:
-    if abs(t - round(t)) < 1e-6: 
-        print(f"t: {t:.2f} s,\t v: {v_func(t)} m/s\t x: {x_func(t)} m")
+    v_lag = v_func(t)
+    x_lag = x_func(t)
+ 
+    print(f"t: {t:.2f} s,\t v: {v_lag} m/s\t x: {x_lag} m")
     
+    v_prev = v_lag
+    x_prev = x_lag
+
     t += dt
