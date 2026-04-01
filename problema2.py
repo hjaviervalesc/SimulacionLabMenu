e2fabd0bb281a4f6caa850106376025057dc7e23
LonchaDeXope <142729532+LonchaDeXope@users.noreply.github.com>
2026-04-01 20:24:22 +0200

    problema 2 comentado y acabado

--- problema2.py
+++ problema2.py
@@ -1,59 +1,86 @@
 import taichi as ti
 import numpy as np
-ti.init(arch=ti.cpu)
 
+#IDEA DEL EJERCICIO
+#El planteamiento es muy similar al apartado anterior
+#Se pide añadir resistencia del aire, más fuerzas afectan a la bola
+#Aceleración no será solo gravedad, calcular aceleración total y aplicar
 
+#Inicializo Taichi
+ti.init(arch=ti.cpu) 
+
+#Discretizo la gravedad
 g = ti.Vector([0.0, -10.0, 0.0])
 a_helper = g
 
-
+#Velocidad y posición en el momento inicial
 v_prev = ti.Vector.field(3, dtype=ti.f32, shape=())
 x_prev = ti.Vector.field(3, dtype=ti.f32, shape=())
 
 v_prev[None] = ti.Vector([10.0, 0.0, 30.0])
 x_prev[None] = ti.Vector([0.0, 100.0, 0.0])
 
+#Parámetros físicos
+m = 1 
+d = 0.4 
 
-m = 1 #Masa
-d = 0.4 #Coeficiente de resistencia al aire
+#Parámetros de la simulación
 total_time = 5
-dt = 1
+
+#Delta Time reducido para poder ver la simulación
+#Para ver resultados del enunciado, poner a 1
+dt = 0.03
 t = 0.0
 
-
-
+##FUNCIONES
+#Cálculo de velocidad mediante Euler
 @ti.func
 def v_func(v, a):
+    #Ahora se usa aceleración total en vez de gravedad
     return v + a * dt
 
+#Cálculo de posición mediante Euler
 @ti.func
 def x_func(x, v):
     return x + v * dt
 
+#Cálculo de aceleración
 @ti.func
 def g_func(v):
     a = g- ((d/m) * v)
     return a
 
+#Función UPDATE del proyecto (ejecutada cada frame)
 @ti.kernel
 def step():
+
+    #Calculo parámetros (Velocidad, Posicion, Gravedad)
     a_helper = g_func(v_prev[None])
+
+    #La velocidad depende de la aceleración total
     v_helper = v_func(v_prev[None], a_helper)
     x_helper = x_func(x_prev[None], v_prev[None])
     
+    #Actualizo valores
     v_prev[None] = v_helper
     x_prev[None] = x_helper
 
+#Reset del programa
 def reset():
     v_prev[None] = ti.Vector([10.0, 0.0, 30.0])
     x_prev[None] = ti.Vector([0.0, 100.0, 0.0])
     t = 0.0
 
+##PROGRAMA PRINCIPAL
 if __name__ == "__main__":
     print(f"t: {t:.2f} s,\t v: {np.round(v_prev[None],1)} m/s\t x: {np.round(x_prev[None],1)} m")
 
+    #Bucle de simulación
     while t < total_time:
+        #Calculo tiempo
         t += dt
-        step()  
+        #Update
+        step() 
+        #Saco resultado 
         print(f"t: {t:.2f} s,\t v: {np.round(v_prev[None],1)} m/s\t x: {np.round(x_prev[None],1)} m")
 
---
+++ simulacion_problema2.py
@@ -0,0 +1,35 @@
+import taichi as ti
+ti.init(arch=ti.cpu)
+import problema2
+
+ball_radius = 0.3
+ball_center = ti.Vector.field(3, dtype=float, shape=(1, ))
+ball_center[0] = [0, 100, 0]
+
+window = ti.ui.Window("Bola 3D", (1024, 1024))
+canvas = window.get_canvas()
+canvas.set_background_color((1, 1, 1))
+scene = window.get_scene()
+
+camera = ti.ui.Camera()
+camera.position(35, 60, 120)
+camera.lookat(0.0, 60, 0)
+scene.set_camera(camera)
+
+initialized= False
+
+while window.running:
+    if window.get_event(ti.ui.PRESS):
+            e = window.event
+            if e.key == "r":
+                problema2.reset()
+
+    problema2.step()
+
+    ball_center[0] = problema2.x_prev[None]
+    
+    scene.point_light(pos=(0, 1, 2), color=(1, 1, 1))
+    scene.ambient_light((0.5, 0.5, 0.5))
+    scene.particles(ball_center, radius=ball_radius, color=(0.5, 0.42, 0.8))
+    canvas.scene(scene)
+    window.show()
