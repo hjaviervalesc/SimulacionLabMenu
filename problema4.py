79de414926bcbd0cdcd4f17749fd693434437f67
AlvaroQ <alvaro.quintanilla@outlook.es>
2026-04-02 13:02:07 +0200

    división

--- problema4.py
+++ problema4.py
@@ -1,5 +1,4 @@
 import taichi as ti
-ti.init(arch=ti.cpu)
 
 #Constantes físicas
 g = ti.Vector([0.0, -10.0, 0.0])
@@ -7,7 +6,7 @@ v_viento = ti.Vector([-12.5, 0.0, 0.0])
 m = 1.0
 d = 0.4
 
-#Parámetros de la simulación
+#Parámetros
 N = 100
 dt = 0.01
 
@@ -22,9 +21,9 @@ def init():
         x_prev[i] = ti.Vector([0.0, 100.0, 0.0])
 
         v_prev[i] = ti.Vector([
-            ti.random() * 100 - 50,   # x [-50, 50]
-            ti.random() * 100,        # y [0, 100]
-            ti.random() * 100 - 50    # z [-50, 50]
+            ti.random() * 100 - 50,
+            ti.random() * 100,
+            ti.random() * 100 - 50
         ])
 
 #Física
@@ -32,23 +31,14 @@ def init():
 def a_func(v):
     return g - (d/m) * (v - v_viento)
 
-@ti.func
-def v_func(v, a):
-    return v + a * dt
-
-@ti.func
-def x_func(x, v):
-    return x + v * dt
-
-# Step Euler
+#Step Euler
 @ti.kernel
 def step():
     for i in range(N):
         a = a_func(v_prev[i])
 
-        # Symplectic Euler
-        v_new = v_func(v_prev[i], a)
-        x_new = x_func(x_prev[i], v_new)
+        v_new = v_prev[i] + a * dt
+        x_new = x_prev[i] + v_new * dt
 
         # suelo
         if x_new[1] < 0:
@@ -58,28 +48,6 @@ def step():
         v_prev[i] = v_new
         x_prev[i] = x_new
 
-#VISUALIZACIÓN
-if __name__ == "__main__":
+#Reset
+def reset():
     init()
-
-    window = ti.ui.Window("Sistema de Partículas", (1024, 1024))
-    canvas = window.get_canvas()
-    canvas.set_background_color((1, 1, 1))
-    scene = window.get_scene()
-
-    camera = ti.ui.Camera()
-    #cámara
-    camera.position(0.0, 100.0, 400.0)
-    camera.lookat(0.0, 100.0, 0.0)
-    scene.set_camera(camera)
-
-    while window.running:
-        step()
-
-        scene.point_light(pos=(0, 100, 200), color=(1, 1, 1))
-        scene.ambient_light((0.5, 0.5, 0.5))
-
-        scene.particles(x_prev, radius=0.9, color=(0.5, 0.42, 0.8))
-
-        canvas.scene(scene)
-        window.show()
---
+++ simulacion_problema4.py
@@ -0,0 +1,40 @@
+import taichi as ti
+ti.init(arch=ti.cpu)
+
+import problema4
+
+N = problema4.N
+
+ball_radius = 0.3
+ball_center = ti.Vector.field(3, dtype=float, shape=(N,))
+
+window = ti.ui.Window("Explosion de Partículas", (1024, 1024))
+canvas = window.get_canvas()
+canvas.set_background_color((1, 1, 1))
+scene = window.get_scene()
+
+camera = ti.ui.Camera()
+camera.position(0.0, 100.0, 300.0)
+camera.lookat(0.0, 100.0, 0.0)
+scene.set_camera(camera)
+
+problema4.init()
+
+while window.running:
+
+    if window.get_event(ti.ui.PRESS):
+        if window.event.key == "r":
+            problema4.reset()
+
+    problema4.step()
+
+    for i in range(N):
+        ball_center[i] = problema4.x_prev[i]
+
+    scene.point_light(pos=(0, 100, 200), color=(1, 1, 1))
+    scene.ambient_light((0.5, 0.5, 0.5))
+
+    scene.particles(ball_center, radius=ball_radius, color=(0.5, 0.42, 0.8))
+
+    canvas.scene(scene)
+    window.show()
